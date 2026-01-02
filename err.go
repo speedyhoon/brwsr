02b81b26d39bf2747b82cec0afe8671600a3a496
sh <16303791+speedyhoon@users.noreply.github.com>
2026-01-02 18:01:16 +1100

    Improve package description.

--- err.go
+++ err.go
@@ -1,4 +1,4 @@
-// Package brwsr implements a way to open links in the user's default browser compatible with most operating systems.
+// Package brwsr implements a way to open URL links in the user's default browser, compatible with most operating systems.
 package brwsr
 
 const errMsg = "unable to open web browser for URL `%s`:\n%w"
